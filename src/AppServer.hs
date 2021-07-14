abec285e5af04d8d0bbc8afd05e5ff3b7aabbd33
Andrey Romanov <steampart@gmail.com>
2021-07-14 21:27:04 +0300

    Project documentation (#11)
    * finished test for all functionality. changed post method to get, because no support for headers in tests during post method execution
    
    * documentation finished
    
    * removed images folder

--- src/AppServer.hs
+++ src/AppServer.hs
@@ -11,7 +11,7 @@
 module AppServer where
 
 import BlockchainService (createEmptyChainFile, listBalances, mineAndSaveBlock)
-import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson (ToJSON)
 import GHC.Generics (Generic)
 import Servant
   ( Application,
@@ -30,6 +30,7 @@ import Servant
 import System.IO.Unsafe (unsafePerformIO)
 import Types (Account (..))
 
+-- dto for handling balance list information
 data BalanceList = BalanceList
   { account :: Integer,
     amount :: Integer
@@ -38,36 +39,32 @@ data BalanceList = BalanceList
 
 instance ToJSON BalanceList
 
-data BlockMineInfo = BlockMineInfo
-  { fileName :: String,
-    accountValue :: String
-  }
-  deriving (Eq, Show, Generic)
-
-instance FromJSON BlockMineInfo
-
-instance ToJSON BlockMineInfo
-
+-- entity for handling mine process of block
 data BlockMineMessage = BlockMineMessage
   {message :: String}
   deriving (Eq, Show, Generic)
 
 instance ToJSON BlockMineMessage
 
+-- definition of http api for service
 type API =
   "listBalancesRoute"
     :> QueryParam "file_name" String
     :> Get '[JSON] [BalanceList]
     :<|> "mineBlocksRoute"
-      :> ReqBody '[JSON] BlockMineInfo
-      :> Post '[JSON] BlockMineMessage
+      :> QueryParam "file_name" String
+      :> QueryParam "account_value" String
+      :> Get '[JSON] BlockMineMessage
     :<|> "createChainFileRoute"
       :> QueryParam "file_name" String
       :> Get '[JSON] BlockMineMessage
 
+--convertation of tuple to BalanceList entity
 convert :: (Account, Integer) -> BalanceList
 convert (Account accValue, amountValue) = BalanceList accValue amountValue
 
+-- appServer with implementation for each route
+-- used unsafePerformIO, because didn't find, how to return IO in Servant HTTP Route
 appServer :: Server API
 {-# NOINLINE appServer #-}
 appServer = listBalancesRoute :<|> mineBlocksRoute :<|> createChainFileRoute
@@ -81,9 +78,14 @@ appServer = listBalancesRoute :<|> mineBlocksRoute :<|> createChainFileRoute
         accountWithBalances = unsafePerformIO (listBalances fileNameValue)
         result = map convert accountWithBalances
 
-    mineBlocksRoute :: BlockMineInfo -> Handler BlockMineMessage
-    mineBlocksRoute (BlockMineInfo _fileName _accountValue) = return result
+    mineBlocksRoute :: Maybe String -> Maybe String -> Handler BlockMineMessage
+    mineBlocksRoute fileNameOpt accountValueOpt
+      | fileNameOpt == Nothing || accountValueOpt == Nothing = return (BlockMineMessage "no such file chain")
+      | fileNameOpt == Nothing && accountValueOpt == Nothing = return (BlockMineMessage "no such file chain")
+      | otherwise = return result
       where
+        (Just _fileName) = fileNameOpt
+        (Just _accountValue) = accountValueOpt
         minedMessage = unsafePerformIO (mineAndSaveBlock _fileName _accountValue)
         result = BlockMineMessage minedMessage
 
